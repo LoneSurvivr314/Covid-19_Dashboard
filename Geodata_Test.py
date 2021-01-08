1893c078ef44ecaf10541eee3ca445d5e977d8d2
LoneSurvivr314 <LoneSurvivr314@users.noreply.github.com>
2021-01-08 10:32:06 -0700

    Changed Some Stuff

--- Geodata_Test.py
+++ Geodata_Test.py
@@ -1,6 +1,6 @@
 #import libraries
 from bokeh.plotting import figure, show, output_file
-from bokeh.models import ColumnDataSource, GeoJSONDataSourceHoverTool
+from bokeh.models import ColumnDataSource, GeoJSONDataSource, HoverTool
 from bokeh.tile_providers import CARTODBPOSITRON_RETINA, WIKIMEDIA, STAMEN_TERRAIN_RETINA, get_provider
 import pandas as pd
 import numpy as np
@@ -8,4 +8,4 @@ import random as rand
 import geopandas as gpd
 
 shapefile = gpd.read_file(r'C:\Users\BUILD-01\Documents\GitHub\Covid-19_Dashboard\World_Countries__Generalized_-shp\World_Countries__Generalized_.shp')
-print(shapfile)
+print(type(shapefile['geometry'][1])
